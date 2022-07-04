3afe27cc916674b3929492e0a661cc88625cd5ac
samuki <s.kiegeland@gmx.de>
2022-07-04 18:29:21 +0200

    use config

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
+import utils
+import torch
+
 PATCH_SIZE = 16  # pixels per side of square patches
 VAL_SIZE = 10  # size of the validation set (number of images)
 CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
-
+BATCH_SIZE = 4
 HEIGHT = 384
 WIDTH = 384
 MINHEIGHT=400
 MINWIDTH=400
+
+EPOCHS =10
+
+METRICS = {'acc': utils.accuracy_fn, 'patch_acc': utils.patch_accuracy_fn}
+LOSS = torch.nn.BCELoss()
+OPTIMIZER = torch.optim.Adam(model.parameters())
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from glob import glob
 import cv2
 import numpy as np
 import torch
-from config import CUTOFF, PATCH_SIZE
+import config 
 from train import train
 from unet import UNet
-from utils import accuracy_fn, create_submission, load_all_from_path, np_to_tensor, patch_accuracy_fn
+import utils
 import dataset 
 
 def main():
@@ -13,7 +13,6 @@ def main():
     # reshape the image to simplify the handling of skip connections and maxpooling
     #train_dataset = ImageDataset('training', device, use_patches=False, resize_to=(384, 384))
     #val_dataset = ImageDataset('validation', device, use_patches=False, resize_to=(384, 384))
-    
     # TODO try using two classes
     #class_names = ['background', 'street']
     class_names = ['street']
@@ -28,7 +27,7 @@ def main():
         'training', 
         device,
         use_patches=False,
-        resize_to=(384, 384),
+        resize_to=(config.HEIGHT, config.WIDTH),
         augmentation=dataset._training_augmentation(),
         preprocessing=dataset._flexible_preprocess(),
         select_class_rgb_values=select_class_rgb_values
@@ -38,39 +37,37 @@ def main():
         'validation', 
         device,
         use_patches=False,
-        resize_to=(384, 384),
+        resize_to=(config.HEIGHT, config.WIDTH),
         augmentation=dataset._validation_augmentation(), 
         preprocessing=dataset._flexible_preprocess(),
         select_class_rgb_values=select_class_rgb_values
     )
-    image, mask = train_dataset[1]
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=2, shuffle=True)
     model = UNet().to(device)
-    loss_fn = torch.nn.BCELoss()
-    metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
+    loss_fn = config.LOSS
+    metric_fns = config.METRICS
     optimizer = torch.optim.Adam(model.parameters())
-    n_epochs = 10
+    n_epochs = config.EPOCHS
     train(train_dataloader, val_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs)
     test_path = 'test/images'
     # predict on test set
     test_filenames = (glob(test_path + '/*.png'))
-    test_images = load_all_from_path(test_path)
-    batch_size = test_images.shape[0]
+    test_images = utils.load_all_from_path(test_path)
     size = test_images.shape[1:3]
     # we also need to resize the test images. This might not be the best ideas depending on their spatial resolution.
     test_images = np.stack([cv2.resize(img, dsize=(384, 384)) for img in test_images], 0)
     test_images = test_images[:, :, :, :3]
-    test_images = np_to_tensor(np.moveaxis(test_images, -1, 1), device)
+    test_images = utils.np_to_tensor(np.moveaxis(test_images, -1, 1), device)
     test_pred = [model(t).detach().cpu().numpy() for t in test_images.unsqueeze(1)]
     test_pred = np.concatenate(test_pred, 0)
     test_pred= np.moveaxis(test_pred, 1, -1)  # CHW to HWC
     test_pred = np.stack([cv2.resize(img, dsize=size) for img in test_pred], 0)  # resize to original shape
     # now compute labels
-    test_pred = test_pred.reshape((-1, size[0] // PATCH_SIZE, PATCH_SIZE, size[0] // PATCH_SIZE, PATCH_SIZE))
+    test_pred = test_pred.reshape((-1, size[0] // config.PATCH_SIZE, config.PATCH_SIZE, size[0] // config.PATCH_SIZE, config.PATCH_SIZE))
     test_pred = np.moveaxis(test_pred, 2, 3)
-    test_pred = np.round(np.mean(test_pred, (-1, -2)) > CUTOFF)
-    create_submission(test_pred, test_filenames, submission_filename='unet_submission.csv')
+    test_pred = np.round(np.mean(test_pred, (-1, -2)) > config.CUTOFF)
+    utils.create_submission(test_pred, test_filenames, submission_filename='unet_submission.csv')
     
     
     
