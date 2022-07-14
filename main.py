9a1cafa642799006b09633371db503ab01cf3180
Christoffer Raun <christoffer.raun@gmail.com>
2022-07-14 16:06:49 +0200

    first version of new unet (not tested)

--- main.py
+++ main.py
@@ -2,68 +2,98 @@ from glob import glob
 import cv2
 import numpy as np
 import torch
-import config 
+import config
 from train import train
 import utils
 from models.unet import UNet
 import dataset
-import datetime 
+import datetime
+
 
 def main():
     # log training
     now = datetime.datetime.now()
     dt_string = now.strftime("results/%d%m%Y_%H:%M:%S")
     utils.make_log_dir(dt_string)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     # reshape the image to simplify the handling of skip connections and maxpooling
     if config.USE_AUGMENTATIONS:
         train_dataset = dataset.FlexibleDataset(
-            'data/training', 
+            "data/training",
             device,
             use_patches=False,
             resize_to=(config.HEIGHT, config.WIDTH),
             augmentation=dataset._training_augmentation(),
         )
         val_dataset = dataset.FlexibleDataset(
-            'data/validation', 
+            "data/validation",
             device,
             use_patches=False,
             resize_to=(config.HEIGHT, config.WIDTH),
             augmentation=dataset._training_augmentation(),
         )
     else:
-        train_dataset = dataset.ImageDataset('data/training', device, use_patches=False, resize_to=(384, 384))
-        val_dataset = dataset.ImageDataset('data/validation', device, use_patches=False, resize_to=(384, 384))
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
+        train_dataset = dataset.ImageDataset(
+            "data/training", device, use_patches=False, resize_to=(384, 384)
+        )
+        val_dataset = dataset.ImageDataset(
+            "data/validation", device, use_patches=False, resize_to=(384, 384)
+        )
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config.BATCH_SIZE, shuffle=True
+    )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=config.BATCH_SIZE, shuffle=True
+    )
     model = UNet().to(device)
     loss_fn = config.LOSS
     metric_fns = config.METRICS
     optimizer = torch.optim.Adam(model.parameters())
     n_epochs = config.EPOCHS
-    train(train_dataloader, val_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs)
-    torch.save(model.state_dict(), dt_string+"/model.pth")
+    train(
+        train_dataloader,
+        val_dataloader,
+        model,
+        loss_fn,
+        metric_fns,
+        optimizer,
+        n_epochs,
+    )
+    torch.save(model.state_dict(), dt_string + "/model.pth")
 
-    test_path = 'data/test/images'
+    test_path = "data/test/images"
     # predict on test set
-    test_filenames = (glob(test_path + '/*.png'))
+    test_filenames = glob(test_path + "/*.png")
     test_images = utils.load_all_from_path(test_path)
     size = test_images.shape[1:3]
     # we also need to resize the test images. This might not be the best ideas depending on their spatial resolution.
-    test_images = np.stack([cv2.resize(img, dsize=(384, 384)) for img in test_images], 0)
+    test_images = np.stack(
+        [cv2.resize(img, dsize=(384, 384)) for img in test_images], 0
+    )
     test_images = test_images[:, :, :, :3]
     test_images = utils.np_to_tensor(np.moveaxis(test_images, -1, 1), device)
     test_pred = [model(t).detach().cpu().numpy() for t in test_images.unsqueeze(1)]
     test_pred = np.concatenate(test_pred, 0)
-    test_pred= np.moveaxis(test_pred, 1, -1)  # CHW to HWC
-    test_pred = np.stack([cv2.resize(img, dsize=size) for img in test_pred], 0)  # resize to original shape
+    test_pred = np.moveaxis(test_pred, 1, -1)  # CHW to HWC
+    test_pred = np.stack(
+        [cv2.resize(img, dsize=size) for img in test_pred], 0
+    )  # resize to original shape
     # now compute labels
-    test_pred = test_pred.reshape((-1, size[0] // config.PATCH_SIZE, config.PATCH_SIZE, size[0] // config.PATCH_SIZE, config.PATCH_SIZE))
+    test_pred = test_pred.reshape(
+        (
+            -1,
+            size[0] // config.PATCH_SIZE,
+            config.PATCH_SIZE,
+            size[0] // config.PATCH_SIZE,
+            config.PATCH_SIZE,
+        )
+    )
     test_pred = np.moveaxis(test_pred, 2, 3)
     test_pred = np.round(np.mean(test_pred, (-1, -2)) > config.CUTOFF)
-    utils.create_submission(test_pred, test_filenames, submission_filename=dt_string+'/submission.csv')
-    
-    
-    
+    utils.create_submission(
+        test_pred, test_filenames, submission_filename=dt_string + "/submission.csv"
+    )
+
+
 if __name__ == "__main__":
     main()
---
+++ models/unet_new.py
@@ -0,0 +1,174 @@
+from glob import glob
+from random import sample
+import torch
+from torch import nn
+from tqdm.notebook import tqdm
+from timm import create_model
+
+# adopted from https://gist.github.com/rwightman/f8b24f4e6f5504aba03e999e02460d31
+
+
+class UNet(nn.Module):
+    # UNet-like architecture for single class semantic segmentation.
+    def __init__(
+        self,
+        backbone="resnet50",
+        backbone_kwargs=None,
+        backbone_indices=None,
+        decoder_use_batchnorm=True,
+        decoder_channels=(256, 128, 64, 32, 16),
+        in_chans=1,
+        num_classes=5,
+        center=False,
+        norm_layer=nn.BatchNorm2d,
+    ):
+        super().__init__()
+
+        backbone_kwargs = backbone_kwargs or {}
+        # NOTE some models need different backbone indices specified based on the alignment of features
+        # and some models won't have a full enough range of feature strides to work properly.
+        encoder = create_model(
+            backbone,
+            features_only=True,
+            out_indices=backbone_indices,
+            in_chans=in_chans,
+            pretrained=True,
+            **backbone_kwargs
+        )
+        encoder_channels = encoder.feature_info.channels()[::-1]
+        self.encoder = encoder
+
+        if not decoder_use_batchnorm:
+            norm_layer = None
+        self.decoder = UnetDecoder(
+            encoder_channels=encoder_channels,
+            decoder_channels=decoder_channels,
+            final_channels=num_classes,
+            norm_layer=norm_layer,
+            center=center,
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.encoder(x)
+        x.reverse()  # torchscript doesn't work with [::-1]
+        x = self.decoder(x)
+        return x
+
+
+class Conv2dBnAct(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        padding=0,
+        stride=1,
+        act_layer=nn.ReLU,
+        norm_layer=nn.BatchNorm2d,
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        )
+        self.bn = norm_layer(out_channels)
+        self.act = act_layer(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        scale_factor=2.0,
+        act_layer=nn.ReLU,
+        norm_layer=nn.BatchNorm2d,
+    ):
+        super().__init__()
+        conv_args = dict(kernel_size=3, padding=1, act_layer=act_layer)
+        self.scale_factor = scale_factor
+        if norm_layer is None:
+            self.conv1 = Conv2dBnAct(in_channels, out_channels, **conv_args)
+            self.conv2 = Conv2dBnAct(out_channels, out_channels, **conv_args)
+        else:
+            self.conv1 = Conv2dBnAct(
+                in_channels, out_channels, norm_layer=norm_layer, **conv_args
+            )
+            self.conv2 = Conv2dBnAct(
+                out_channels, out_channels, norm_layer=norm_layer, **conv_args
+            )
+
+    def forward(self, x, skip: Optional[torch.Tensor] = None):
+        if self.scale_factor != 1.0:
+            x = F.interpolate(x, scale_factor=self.scale_factor, mode="nearest")
+        if skip is not None:
+            x = torch.cat([x, skip], dim=1)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+
+class UnetDecoder(nn.Module):
+    def __init__(
+        self,
+        encoder_channels,
+        decoder_channels=(256, 128, 64, 32, 16),
+        final_channels=1,
+        norm_layer=nn.BatchNorm2d,
+        center=False,
+    ):
+        super().__init__()
+
+        if center:
+            channels = encoder_channels[0]
+            self.center = DecoderBlock(
+                channels, channels, scale_factor=1.0, norm_layer=norm_layer
+            )
+        else:
+            self.center = nn.Identity()
+
+        in_channels = [
+            in_chs + skip_chs
+            for in_chs, skip_chs in zip(
+                [encoder_channels[0]] + list(decoder_channels[:-1]),
+                list(encoder_channels[1:]) + [0],
+            )
+        ]
+        out_channels = decoder_channels
+
+        self.blocks = nn.ModuleList()
+        for in_chs, out_chs in zip(in_channels, out_channels):
+            self.blocks.append(DecoderBlock(in_chs, out_chs, norm_layer=norm_layer))
+        self.final_conv = nn.Conv2d(
+            out_channels[-1], final_channels, kernel_size=(1, 1)
+        )
+
+        self._init_weight()
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x: List[torch.Tensor]):
+        encoder_head = x[0]
+        skips = x[1:]
+        x = self.center(encoder_head)
+        for i, b in enumerate(self.blocks):
+            skip = skips[i] if i < len(skips) else None
+            x = b(x, skip)
+        x = self.final_conv(x)
+        return x
